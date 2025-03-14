b54db00b90c78240f7066e2a8b7579c58f5356bd
N-Hidaya <nurul-h@hotmail.com>
2025-03-15 04:43:24 +0800

    style edits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,11 @@ import React, { useEffect, useState } from 'react';
 const Home = () => {
   return (
     <div className=" top-0 bottom-0 min-h-[600px] " style={{ backgroundImage: "url('/assets/bg-trans.png')", backgroundSize: 'cover'}}>
-    <div className=" justify-center items-center px-6">
+    <div className=" justify-items-center px-6">
         <img src="/assets/icon.png" alt="Logo" className="justify-self-center h-[500px] pt-10 animate-[bounce_2s_ease-in-out_infinite]" />
         <p className="text-lg pb-28 text-center font-[Dynapuff] ">
-        Cherigo is your one-stop shop for all your favorite anime merchandise. From keychains to standees, we have it all!
+        Cherigo is your one-stop shop for all your favorite anime merchandise. <br />
+        From keychains to standees, we have it all!
       </p>
     
     </div>
